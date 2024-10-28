f5a6c4505f250ef324e89946a2de1e54a31e3685
ASarkhel07 <armaansarkhel06@gmail.com>
2024-10-28 14:07:11 -0400

    1st Task Addition - Armaan
    My code for the 1st task for Alvin's repository

--- CollaborationSmindy.java
+++ CollaborationSmindy.java
@@ -2,7 +2,24 @@ public class CollaborationSmindy {
 
 	public static void main(String [] args){
 
+		squareArray(5,5);
 
+	}
+
+	public static void squareArray(int x, int y)
+	{
+		int[][]newArray = new int[x][y];
+
+		for(int i = 0; i<x; i++)
+		{
+			for(int j =0; j<y; j++)
+			{
+				int rand = (int)(Math.random()*10)+1;
+				newArray[i][j] =rand;
+				System.out.print(newArray[i][j] + " ");
+			}
+			System.out.println();
+		}
 	}
 
 }
